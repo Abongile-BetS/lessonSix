e4f807fe8982e6aa49af984484bd338db98eed5b
Abongile-BetS <ntsikag@betsoftware.com>
2026-03-23 11:19:32 +0200

    fourth commit

---
+++ convertArray(4)/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace convertArray_4_
+{
+    internal class Program
+    {
+        
+        public class Employee
+        {
+            public string Name { get; }
+
+            public Employee(string name)
+            {
+                Name = name;
+            }
+
+            public virtual void Work()
+            {
+                Console.WriteLine($"{Name} is working.");
+            }
+        }
+
+        public class Developer : Employee
+        {
+            public Developer(string name) : base(name) { }
+
+            public override void Work()
+            {
+                Console.WriteLine($"{Name} is writing code.");
+            }
+        }
+
+        public class Manager : Employee
+        {
+            public Manager(string name) : base(name) { }
+
+            public override void Work()
+            {
+                Console.WriteLine($"{Name} is managing the team.");
+            }
+        }
+
+        // TODO: Create an Intern class that inherits from Employee
+        public class Intern : Employee
+        {
+            public Intern(string name) : base(name)
+            {
+            }
+            public override void Work()
+            {
+                Console.WriteLine($"{Name} is writing code.");
+                Console.WriteLine($"{Name} is learning and helping with small tasks");
+
+            }
+
+
+        }
+        // TODO: Override Work() to print:
+        public void Display()
+        {
+        }
+        // "{Name} is learning and helping with small tasks."
+        public static void Main()
+        {
+            List<Employee> team = new()
+        {
+            new Developer("Ada"),
+            new Manager("Grace"),
+			// TODO: Add an Intern object here
+            new Intern("Ntsika")
+		};
+
+            foreach (var employee in team)
+            {
+                employee.Work();
+            }
+        }
+    }
+}
+
