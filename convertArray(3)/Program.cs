2317ac9e2421b276d9443765f8f6948f2f9eeda2
Abongile-BetS <ntsikag@betsoftware.com>
2026-03-23 10:48:07 +0200

    third commit

---
+++ convertArray(3)/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace convertArray_3_
+{
+    internal class Program
+    {
+        
+        public class User
+        {
+            private string _username;
+
+            public User(string username)
+            {
+                _username = username;
+            }
+
+            public string Username
+            {
+                get
+                {
+                    return _username;
+                }
+                set
+                {
+                    // TODO:
+                    // 1. Check if value is null, empty, or whitespace
+                    if(string.IsNullOrWhiteSpace(value))
+                    {
+                        Console.WriteLine("Error: Cannot be empty");
+                        return;
+                    }
+                    
+                    _username = value;
+                    // 2. Print an error message and return if invalid
+                    // 3. Otherwise update _username
+                }
+            }
+        }
+        public static void Main()
+        {
+            var user = new User("Witcher01");
+
+            Console.WriteLine($"Starting username: {user.Username}");
+
+            user.Username = "";
+            Console.WriteLine($"After invalid update: {user.Username}");
+
+            user.Username = "WhiteWolf";
+            Console.WriteLine($"After valid update: {user.Username}");
+        }
+    }
+}
